9c878de19a4fa9df7ab4ed8aec474221b37b02a2
ling-ly <75435135+ling-ly@users.noreply.github.com>
2021-04-14 10:45:51 +0800

    修改Hello.java文件
    第二次用Github练习修改文件

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello(){
   public static viod main(String args[]){
     System.out.println("Hello Github");
-    System.out.println("test commit count");
+    System.out.println("test commit  count");
   }
 }

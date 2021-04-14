8ef4c243b3fe980b7275c52eabd14906483f72e3
ling-ly <75435135+ling-ly@users.noreply.github.com>
2021-04-14 11:02:54 +0800

    对电脑上传文件进行修改
    使用Github对电脑上传文件进行操作

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String args[]){
-		System.out.println("¥Ûº“∫√£°");
+		System.out.println("‰Ω†Â•ΩÔºÅ");
 		System.out.println("Nice to meet you");
 		Student stu = new Student();
 		stu.speak("We are student");
